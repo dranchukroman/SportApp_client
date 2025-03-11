3681b8e9fadbabd7be6cf9de2ecd3a7590b5fb0b
dranchukroma <roman2003dranchuk@gmail.com>
2025-03-11 19:33:30 +0100

    Created components for other creating training plan screens

---
+++ src/screens/otherViews/Trainings/SetUpExercises/SetUpExercises.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import axios from "axios";
+
+import Heading from "../../../../components/Headings/Heading";
+import Input from "../../../../components/Inputs/Input";
+import Button from "../../../../components/Buttons/Button";
+import ChooseImageIcon from "../../../../assets/icons/ChooseImageIcon";
+
+function SetUpExercises({ token, onScreenChange}){
+
+    return (
+        <div>
+            <Heading>
+                SetUpExercises
+            </Heading>
+        </div>
+    );
+}
+
+export default SetUpExercises;
