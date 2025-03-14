906241e7e2ca3d505c44fc938cca62a3d9fb5a7d
dranchukroma <roman2003dranchuk@gmail.com>
2025-03-14 15:36:38 +0100

    Fix deployment script for Railway

---
+++ build/precache-manifest.7484a542a3fb9b8c7397a39e24e131a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70ce6089a4148caafcf3980bceb5a0cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16415807af61af33f462",
+    "url": "/static/css/main.9bc4fb54.chunk.css"
+  },
+  {
+    "revision": "fba200360a6edc1b238f",
+    "url": "/static/js/2.7152b768.chunk.js"
+  },
+  {
+    "revision": "0887163a8605841c3eb9334a885cc238",
+    "url": "/static/js/2.7152b768.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16415807af61af33f462",
+    "url": "/static/js/main.627f99dc.chunk.js"
+  },
+  {
+    "revision": "05fbb5ed970a924e23b7",
+    "url": "/static/js/runtime-main.81534a39.js"
+  }
+]);
