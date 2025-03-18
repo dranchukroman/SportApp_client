9715461560908e1172d52b60d619d39ed4a6dedc
dranchukroma <roman2003dranchuk@gmail.com>
2025-03-18 13:01:11 +0100

    Fix for CheckBoxes

--- src/components/Inputs/CheckBoxes/CheckBox.jsx
+++ src/components/Inputs/CheckBoxes/CheckBox.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import { StyledCheckbox, StyledCheckboxWrapper, StyledCheckboxLabel } from './CheckBox.styled';
 
 
-function CheckBox( onChange, style, ischecked = false){
+function CheckBox({ onClick, style, checked }){
     return (
         <StyledCheckboxWrapper style={style}>
             <StyledCheckbox
-                ischecked={ischecked}
-                onChange={onChange}
+                checked={checked}
+                onClick={onClick}
             />
             <StyledCheckboxLabel />
         </StyledCheckboxWrapper>
